f2be8f796641b496c1973468497a9a16008b6152
Adan Martinez <adanvmartinez@gmail.com>
2022-07-20 15:29:09 -0500

    Public IPs

--- subnets.tf
+++ subnets.tf
@@ -3,6 +3,7 @@
 resource "aws_subnet" "terraform-lab-subnet" {
     vpc_id = aws_vpc.terraform-lab-vpc.id
     cidr_block = "10.0.1.0/24" //CIDR block must be string
+    map_public_ip_on_launch = true
     availability_zone = "us-west-1a"
     tags = {
         "Name" = "terraform-lab-subnet1"
@@ -14,6 +15,7 @@ resource "aws_subnet" "terraform-lab-subnet2" {
     vpc_id = aws_vpc.terraform-lab-vpc.id
     cidr_block = "10.0.2.0/24" //CIDR block must be string
     availability_zone = "us-west-1c"
+    map_public_ip_on_launch = true
     tags = {
         "Name" = "terraform-lab-subnet2"
     }
