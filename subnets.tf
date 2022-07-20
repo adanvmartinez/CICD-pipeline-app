6025764226657a396d6aeda06f63bc41fcc06633
bhagwatsapkota <sapkotabhagwat@gmail.com>
2022-07-20 15:43:46 -0400

    1

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 
 //Creation of the subnet we will be working with in AWS
 resource "aws_subnet" "terraform-lab-subnet" {
-    vpc_id = aws_vpc.terraform-lab-vpc.vpc.id
+    vpc_id = aws_vpc.terraform-lab-vpc.id
     cidr_block = "10.0.1.0/24" //CIDR block must be string
     availability_zone = "us-west-1a"
     tags = {
