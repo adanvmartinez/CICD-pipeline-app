c0d5d566cf471355154940a06480d6584d219f58
Adan Martinez <adanvmartinez@gmail.com>
2022-07-20 09:34:26 -0500

    Fixed quotes error and added comments for readability

--- routeTable.tf
+++ routeTable.tf
@@ -1,3 +1,7 @@
+
+
+
+//Creation of route table
 resource "aws_route_table" "terraform-lab-rt" {
     vpc_id = aws_vpc.terraform-lab-vpc.vpc_id
 
@@ -6,17 +10,19 @@ resource "aws_route_table" "terraform-lab-rt" {
         gateway_id = aws_internet_gateway.terraform-lab-gateway.id
     }
     tags = {
-        "Name" = "terraform-lab-rt
+        "Name" = "terraform-lab-rt"
     }
 }
 
 
+//Assigns the route table to the subnet in vpc
 resource "aws_route_table_association" "terraform-lab-subnet-association" {
   subnet_id = aws_subnet.terraform-lab-subnet.id
   route_table_id = aws_route_table.terraform-lab-rt.id
   
 }
 
+//Sets the main route table tot the newly created route table
 resource "aws_main_route_table_association" "terraform-lab-mainRT-association" {
   vpc_id = aws_vpc.terraform-lab-vpc.id
   route_table_id = aws_route_table.terraform-lab-rt.id
