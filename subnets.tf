a880d25896ccc643113437f41a1114b28ba977cb
Adan Martinez <adanvmartinez@gmail.com>
2022-07-20 15:18:47 -0500

    Comments for readability

--- subnets.tf
+++ subnets.tf
@@ -9,6 +9,7 @@ resource "aws_subnet" "terraform-lab-subnet" {
     }
 }
 
+//Creating second  subnet as EKS klusters require availability in at least two Availability Zone
 resource "aws_subnet" "terraform-lab-subnet2" {
     vpc_id = aws_vpc.terraform-lab-vpc.id
     cidr_block = "10.0.2.0/24" //CIDR block must be string
